fb3486edf4e5f14714bfe08805d9ee7dd1a8815f
Zemlyanskiy Nikita <nikita.nikita.000035@gmail.com>
2018-12-21 02:51:25 +0300

    Connect success, add interface to override procedures

--- src/programbrain/Procedure.java
+++ src/programbrain/Procedure.java
@@ -1,4 +1,10 @@
 package programbrain;
+<<<<<<< HEAD
+=======
+import org.postgresql.jdbc2.optional.ConnectionPool;
+
+import java.sql.*;
+>>>>>>> Connect success, add interface to override procedures
 import java.io.PrintWriter;
 
 import static java.lang.System.out;
@@ -6,7 +12,50 @@ import static java.lang.System.out;
 public class Procedure implements ProcedureInterface {
     @Override
     public void ReadChat(int userId, PrintWriter refCursor) {
+<<<<<<< HEAD
         
+=======
+        /*Connection con = null;
+        CallableStatement toesUp = null;
+        Statement stmt = null;
+
+        try
+        {
+            con = ConnectionPool.getConnection();
+            stmt = con.createStatement(  );
+            // для PostgreSQL сначала нужно создать транзакцию (AutoCommit == false)...
+            con.setAutoCommit(false);
+
+            // Настраиваем вызов.
+            toesUp = con.prepareCall("{ ? = call ReadChat (userId, refCursor) }");
+            toesUp.registerOutParameter(1, Types.OTHER);
+            ResultSet getResults = toesUp.getResultSet();
+            stmt.execute();
+
+            ResultSet rs = (ResultSet) getResults.getObject(1);
+            while (rs.next())
+            {
+                String name = rs.getString(1);
+                int age = rs.getInt(2);
+                out.println(name + " was " + age + " years old.");
+            }
+            rs.close();
+        }
+        catch (SQLException e)
+        {
+            // Мы должны защитить эти вызовы.
+            try {
+                toesUp.close();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+            try {
+                con.close();
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+        }*/
+>>>>>>> Connect success, add interface to override procedures
     }
 
     @Override
