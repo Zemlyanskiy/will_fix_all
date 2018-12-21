3c0d0340b6bec65acee0d71e39c10cef364aa55c
Zemlyanskiy <nikita.nikita.000035@gmail.com>
2018-12-21 10:35:05 +0300

    DataBase_Connection (#39)
    * Resolve conflicts with travis
    
    * Resolve conflicts with team reviewing

---
+++ src/programbrain/Procedure.java
@@ -0,0 +1,36 @@
+package programbrain;
+import java.io.PrintWriter;
+
+import static java.lang.System.out;
+
+public class Procedure implements ProcedureInterface {
+    @Override
+    public void ReadChat(int userId, PrintWriter refCursor) {
+        
+    }
+
+    @Override
+    public void ReadRepair(int userId, PrintWriter refCursor) {
+
+    }
+
+    @Override
+    public void ReadUser(int userId, PrintWriter refCursor) {
+
+    }
+
+    @Override
+    public void WriteChat(int idOrder, int userId, String message) {
+
+    }
+
+    @Override
+    public void WriteReoair(int userId, int idOrder, String date, String time, String comment, int type) {
+
+    }
+
+    @Override
+    public void WriteUser(int userId, int idOrder, String email, String password, int type) {
+
+    }
+}
