7d30cdbbb0fe6b63c487b037c1e5ae227309be21
Kochin <kochin.ivan@intel.com>
2018-12-28 03:49:40 +0300

    Create default NetBeans project and change GUI structure

--- NetBeansGUI/src/Test/Test.java
+++ NetBeansGUI/src/Test/Test.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package NetBeansGUI;
+package Test;
 
 import client.Client;
 import client.ClientInterface;
